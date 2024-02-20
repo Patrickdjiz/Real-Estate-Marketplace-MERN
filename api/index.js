3706c91276db44aa3cd13472f42d37dc9d0aa488
Patrickdjiz <djizpatrick@gmail.com>
2024-02-20 15:27:30 +1100

    x

--- api/index.js
+++ api/index.js
@@ -5,13 +5,13 @@ import dotenv from 'dotenv';
 dotenv.config()
 const app = express()
 
-mongoose.connect(process.env.MONGO)
+mongoose.connect(process.env.MONGO_URI)
     .then(() => {
     console.log('Database connected')
+    // we only want the app to connect to our Port if the database is connected
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000')
+    })
 }).catch((err) => {
     console.log(err)
-})
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
 })
