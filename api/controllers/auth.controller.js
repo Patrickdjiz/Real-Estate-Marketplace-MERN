149201d1be1828678cf5cbe34e7215056f72a619
Patrickdjiz <djizpatrick@gmail.com>
2024-02-20 20:57:05 +1100

    x

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js"
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body
     const hashedPassword = bcryptjs.hashSync(password, 10) // the number on the end is the number of rounds of hashing called salt which encrypts the password
     const newUser = new User({ username, email, hashedPassword })
@@ -9,6 +10,6 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.status(201).json(newUser)
     } catch (error) {
-        res.status(409).json(error.message)
+        next(errorHandler(404, 'error in signing up')) // this will send the error to the middleware
     }
 }
--- api/index.js
+++ api/index.js
@@ -13,7 +13,18 @@ app.use(express.json())
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 
-// Middleware
+//--------Middleware---------//
+// our controllers send the error to next() and this middleware will catch it
+app.use((err, req, res, next) => { 
+    const statusCode = err.statusCode || 500 // if there is no statusCode in the error just use 500 as default
+    const message = err.message || 'Internal Server Error' // if there is no message in the error just use 'Internal Server Error' as default
+    return res.status(statusCode).json({ 
+        success: false,
+        statusCode,
+        message,
+     })
+})
+
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
     console.log('Database connected')
