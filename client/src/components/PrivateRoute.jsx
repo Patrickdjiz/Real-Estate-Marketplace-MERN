2de58c6f832bf7a09abbb19df37756f97dc5ee08
Patrickdjiz <djizpatrick@gmail.com>
2024-02-25 14:01:20 +1100

    x

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { UseSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import { Outlet, Navigate } from 'react-router-dom'
 
 export default function PrivateRoute() {
     const {currentUser} = useSelector((state) => state.user) // we use the useSelector hook to get the currentUser state from the 'user' slice
-  return currentUser ? <Outlet/> : <Navigate to='sign-in' /> // if a user is not logged in, going to the profile page will redirect them to the sign-in page
+    return currentUser ? <Outlet/> : <Navigate to='sign-in' /> // if a user is not logged in, going to the profile page will redirect them to the sign-i 
 }
