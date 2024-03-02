351092a7bc11726c20dfc29895c97b93511a100e
Patrickdjiz <djizpatrick@gmail.com>
2024-03-02 15:38:26 +1100

    x

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,6 +1,6 @@
 import Listing from '../models/listing.model.js'
 
-export default createListing = async (req, res, next) => {
+export const createListing = async (req, res, next) => {
     try {
         const listing = await Listing.create(req.body) // we create a new listing with the data from the request
         return res.status(201).json(listing) // we send the listing in the response
