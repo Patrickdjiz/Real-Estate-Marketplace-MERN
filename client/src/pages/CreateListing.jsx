c7999c0a4132bd47e3951696dc8bce8a347d9614
Patrickdjiz <djizpatrick@gmail.com>
2024-03-04 18:02:55 +1100

    x

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,6 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { app } from '../firebase'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import { set } from 'mongoose'
+
 
 export default function CreateListing() {
+    const [files, setFiles] = useState([])
+    const [formData, setFormData] = useState({ // we create a formData object to store the form data that the user will upload as an image
+        imageUrls: [], // we create an imageUrls array to store the download urls of the images
+    })  
+    const [imageUploadError, setImageUploadError] = useState(false) // we create an imageUploadError state to handle errors that occur during the image upload
+    const [uploading, setUploading] = useState(false) 
+    console.log(formData)
+
+    const handleImageSubmit =  (e) => {
+        if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+            setUploading(true)
+            setImageUploadError(false)
+            const promises = [] // promises are used to handle asynchronous operations since the file upload is asynchronous and takes time
+            for (let i = 0; i < files.length; i++) {
+                promises.push(storeImage(files[i])) // we push the promise returned by the storeImage function to the promises array
+            }
+            Promise.all(promises).then((urls) => { // we use the Promise.all method to wait for all the promises to resolve. .then returns the resolve or reject value of the promise
+                setFormData({...formData, imageUrls: formData.imageUrls.concat(urls)}) // we set the imageUrls property of the formData object to the urls array
+                setImageUploadError(false) 
+                setUploading(false)
+            }).catch((error) => {
+                setImageUploadError('Image upload failed')
+            })
+        } else {
+            setImageUploadError('You can only upload 6 images')
+            setUploading(false)
+        }
+    }
+
+    const storeImage = async (file) => {
+        return new Promise((resolve, reject) => { // we create a new promise which takes two parameters, resolve and reject. These are functions that are passed to the promise by JavaScript
+            const storage = getStorage(app) // we get the storage from the app
+            const fileName = new Date().getTime() + file.name // we create a unique file name
+            const storageRef = ref(storage, fileName) // we create a reference to the storage  
+            const uploadTask = uploadBytesResumable(storageRef, file) // we upload the file to the storage from the reference to the storage 
+            uploadTask.on(
+                'state_changed', // we listen to the state of the upload task
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100 // we calculate the progress of the upload
+                    console.log('Upload is ' + progress + '% done') // we log the progress of the upload
+                },
+                (error)=> {
+                    reject(error) // if there is an error we reject the promise
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => { // we get the download url of the file
+                        resolve(downloadURL) // if the upload is successful we resolve the promise with the download url
+                    })
+                }
+            )
+
+        })
+    }
+
+    const handleRemoveImage = (index) => {
+        setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.filter((_, i) => i !== index) // we filter the imageUrls array to remove the image at the index
+        })
+    }
+
   return (
     <main className='p-3 max-w-4xl mx-auto'>
         <h1 className='text-3xl font-semibold text-center my-7'>Create a Listing</h1>
@@ -61,9 +126,22 @@ export default function CreateListing() {
                 <span className='font-normal text-gray-700 ml-2'>The first image will be the cover (max 6)</span>
                 </p>
                 <div className='flex gap-4'>
-                    <input className='p-3 border border-gray-300 rounded w-full' type="file" id='images' accept='image/*' multiple required></input>
-                    <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
+                    <input onChange={(e) => setFiles(e.target.files)} className='p-3 border border-gray-300 rounded w-full' type="file" id='images' accept='image/*' multiple required></input>
+                    <button type='button' onClick={handleImageSubmit} disabled={uploading}
+                    className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>
+                        {uploading ? 'Uploading...' : 'Upload'}
+                    </button>
                 </div>
+                <p className='text-red-700 text-sm'>{imageUploadError && imageUploadError}</p>
+                {
+                    formData.imageUrls.length > 0 && formData.imageUrls.map((url, index) => (
+                        <div key={url} className='flex justify-between p-3 border items-center'>
+                            <img src={url} alt='listing image' className='w-20 h-20 object-contain rounded-lg'></img>
+                            <button type='button' onClick={() => handleRemoveImage(index)} className='p-3 text-red-700 rounded-lg uppercase hover:opacity-75'>Delete</button>
+                        </div>
+                    ))
+
+                }
                 <button className='p-3 bg-slate-700 rounded-lg text-white uppercase hover:opacity-95 disabled:opacity-80'>Create Listing</button>
             </div>
         </form>
