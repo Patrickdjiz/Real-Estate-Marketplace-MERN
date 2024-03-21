82724bbcd8cc99e61d73d1a2c74e256a56f1e113
Patrickdjiz <djizpatrick@gmail.com>
2024-03-21 14:19:46 +1100

    x

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from '../models/listing.model.js'
+import { errorHandler } from '../utils/error.js'
 
 export const createListing = async (req, res, next) => {
     try {
@@ -7,4 +8,22 @@ export const createListing = async (req, res, next) => {
     } catch (error) {
         next(error)
     }
+}
+
+export const deleteListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id) // we find the listing by id
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not found')) // if the listing doesn't exist we send a 404 status and a message
+    }
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(403, 'You are not authorized to delete this listing')) // if the user is not the owner of the listing we send a 403 status and a message
+    }
+
+    try {
+        await Listing.findByIdAndDelete(req.params.id) // we delete the listing
+        return res.status(200).json({ success: true, message: 'Listing deleted' }) // we send a success message
+    } catch (error) {
+        next(error)
+    }
+
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -136,6 +136,22 @@ export default function Profile() {
     }
 
   }
+
+  const handleListingDelete = async (listingId) => {
+    try{
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      })
+      const data = await res.json()
+      if (data.success === false) {
+        console.log(data.message)
+        return
+      }
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId)) // we filter the listings to remove the one we deleted
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
   
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -192,7 +208,7 @@ export default function Profile() {
             </Link>
 
             <div className='flex flex-col items-center'>
-              <button className='text-red-700 uppercase'>Delete</button>
+              <button onClick={() => handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
               <button className='text-green-700 uppercase'>Edit</button>
             </div>
 
