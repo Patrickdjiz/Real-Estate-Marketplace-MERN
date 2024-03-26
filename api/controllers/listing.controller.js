28156e0875df1eeed4435b246a1a909f9a7c572a
Patrickdjiz <djizpatrick@gmail.com>
2024-03-26 20:33:27 +1100

    x

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -27,3 +27,25 @@ export const deleteListing = async (req, res, next) => {
     }
 
 }
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id) // we find the listing by id
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not found')) // if the listing doesn't exist we send a 404 status and a message
+    }
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(403, 'You are not authorized to update this listing')) // if the user is not the owner of the listing we send a 403 status and a message
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id, // we find the listing by id
+            req.body, // we update the listing with the data from the request
+            { new: true } // we set new to true so we get the updated listing in the response
+        ) // we update the listing with the data from the request
+        return res.status(200).json(updatedListing) // we send the updated listing in the response
+    } catch (error) {
+        next(error)
+    }
+
+}
